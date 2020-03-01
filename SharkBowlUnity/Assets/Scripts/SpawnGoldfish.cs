9a9d7f7ca28ba30dc00c24c0e2ce4e62a7284ce8
Rhosyn1 <ct229252@falmouth.ac.uk>
2020-03-01 20:26:14 +0000

    Fixed gitignore, started scripts for game. Goldfish spawns and meter goes up.

---
+++ SharkBowlUnity/Assets/Scripts/SpawnGoldfish.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGoldfish : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject goldfishPrefab;
+
+    private float xMin = -8f;
+    private float xMax = 0f;
+    private float yMin = -4f;
+    private float yMax = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(goldfishPrefab, new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0f), Quaternion.identity);
+        }
+    }
+}
