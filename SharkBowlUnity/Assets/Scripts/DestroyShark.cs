870f32d1b5cbdde402e7ae0f967f6fd119d31142
Rhosyn1 <ct229252@falmouth.ac.uk>
2020-04-26 19:51:02 +0100

    Linked arduino code with unity.

--- SharkBowlUnity/Assets/Scripts/DestroyShark.cs
+++ SharkBowlUnity/Assets/Scripts/DestroyShark.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DestroyShark : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class DestroyShark : MonoBehaviour
         if (uiBehaviour2.slider.value == 1f)
         {
             Destroy(gameObject);
+            SceneManager.LoadScene(1);
         }
     }
  
--- SharkBowlUnity/Assets/Scripts/SpawnGoldfish.cs
+++ SharkBowlUnity/Assets/Scripts/SpawnGoldfish.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using System.IO.Ports;
+
 
 public class SpawnGoldfish : MonoBehaviour
 {
@@ -17,19 +19,55 @@ public class SpawnGoldfish : MonoBehaviour
     private float yMin = -4f;
     private float yMax = 4f;
 
+   /* SerialPort sp = new SerialPort("COM5", 9600);
+    private float scaleValue = 0f;
+    private float lastStableWeight = 0f;
+    private float lastWeight = 0f;
+    private int stableCount = 0;
+    private float offsetWeight = 0.1f;
+    private float fishWeight = 0.2f; */
     void Start()
     {
         uiBehaviour1 = FindObjectOfType<UIBehaviour>();
+        //decrease the hunger meter
         InvokeRepeating("NoGoldfishSpawned", timeToDecrease, timeToDecrease);
+       /* sp.Open();
+        sp.ReadTimeout = 1000;
+        //read value from arduino 
+        scaleValue = float.Parse(sp.ReadLine(), System.Globalization.NumberStyles.Float);
+        lastStableWeight = scaleValue;
+        lastWeight = scaleValue;*/
     }
 
     // Update is called once per frame
     void Update()
     {
+        //trying to get a stable weight so that we can check the change in weight
+       /* scaleValue = float.Parse(sp.ReadLine(), System.Globalization.NumberStyles.Float);
+        if (Mathf.Abs(scaleValue - lastWeight) <= offsetWeight)
+        {
+            stableCount++;
+        }
+        else
+        {
+            stableCount = 0;
+        }
+
+        if (stableCount >= 30)
+        {
+            //adding fish if the change in weight is large enough
+            if (Mathf.Abs(scaleValue - lastStableWeight) >= fishWeight)
+            {
+                Instantiate(goldfishPrefab, new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0f), Quaternion.identity);
+                lastStableWeight = scaleValue;
+            }
+        }*/
         if (Input.GetButtonDown("Jump"))
         {
             Instantiate(goldfishPrefab, new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0f), Quaternion.identity);
         }
+
+        //lastWeight = scaleValue;
     }
 
     private void NoGoldfishSpawned()
